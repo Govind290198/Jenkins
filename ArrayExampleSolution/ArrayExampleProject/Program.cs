c38b86a5b339628870c205c1fd00aa7eb93f05b8
Govind290198 <68836442+Govind290198@users.noreply.github.com>
2020-07-28 12:49:54 +0530

    Add files via upload

---
+++ ArrayExampleSolution/ArrayExampleProject/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExampleProject
+{
+    class Program
+    {
+        //int num1, num2, num3, num4;
+        //int[] numbers = new int[4];
+        int[] numbers;
+        Program()
+        {
+            int size = 0;
+            Console.WriteLine("Enter the numbers");
+            size = Convert.ToInt32(Console.ReadLine());
+            numbers = new int[size];
+        }
+
+        public void TakeNumbersFromUSer()
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("Enter number{0}", (i + 1));
+                while (!Int32.TryParse(Console.ReadLine(), out numbers[i])) ;
+                {
+                    Console.WriteLine("Invalid");
+                }
+            }
+        }
+        public void PrintAllNumbers()
+        {
+            /* for (int i = 0; i < 4; i++)
+             {
+                 Console.WriteLine(numbers[i]);
+            
+        }*/
+        
+
+            
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+
+        }
+
+        void SortArray()
+        {
+            int temp = 0;
+            for (int i=0;i<numbers.Length;i++)
+            {
+                for (int j=0;j<numbers.Length-1;j++)
+                {
+                    if(numbers[j]>numbers[j+1])
+                    {
+                        temp = numbers[j + 1];
+                        numbers[j + 1] = numbers[j];
+                        numbers[j] = temp;
+                    }
+                    
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+                Program program = new Program();
+                program.TakeNumbersFromUSer();
+                program.PrintAllNumbers();
+                Console.ReadKey();
+        }
+    }
+}
---
+++ ArrayExampleSolution/ArrayExampleProject/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExampleProject
+{
+    class Student
+    {
+        string[] skills;
+        string name, phone;
+        int id;
+
+        //indexer - allow object of class to be treated as array
+        void SomeMethod(int id)
+        {
+            //local variable take priority
+            if(this.id==id)
+            {
+                Console.WriteLine("Same Person");
+            }
+        }
+        public string this[int index]
+        {
+
+        }
+        //public string[] Skills { get => skills; set => skills = value; }
+        public string Name { get => name; set => name = value; }
+        public string Phone { get => phone; set => phone = value; }
+        public int Id { get => id; set => id = value; }
+
+        public void GetStudentDetails()
+        {
+            Console.WriteLine("Enter id");
+            while(!Int32.TryParse(Console.ReadLine(),out id))
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+
+
+    }
+
+        
+}
